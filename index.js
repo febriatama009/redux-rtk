80c3e442522299d6f6196fad8c369a52a33628fa
febriatama009 <febriatama009@gmail.com>
2023-03-27 15:23:03 +0700

    Final Code redux-rtk

---
+++ index.js
@@ -0,0 +1,23 @@
+const store = require("./app/store");
+const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const iceCreamActions =
+  require("./features/icecream/iceCreamSlice").iceCreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
+
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated State : ", store.getState());
+});
+
+store.dispatch(fetchUsers());
+
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(2));
+
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(2));
+
+// unsubscribe();
